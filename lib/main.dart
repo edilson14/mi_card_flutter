be0bb651a64db4b5c41ee0884ea625b4b1eff161
Edilson <edytavares66@gmail.com>
2020-02-12 11:21:58 -0300

    add and customize external fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,43 @@ class MiCard extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.white,
         body: SafeArea(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50.0,
-              backgroundColor: Colors.black,
+              backgroundColor: Colors.red,
               backgroundImage: AssetImage('images/profile.jpeg'),
             ),
             Text(
               'Edilson',
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold),
+                color: Colors.black87,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Mobile && Web Developer',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'SourceSansPro',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Learning Dart Flutter',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontFamily: 'Source Sans Pro',
+                fontSize: 10.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         )),
