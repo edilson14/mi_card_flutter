87ffdf715b020941c28b3cf788e7ee5d0dc25209
Edilson <edytavares66@gmail.com>
2020-02-13 10:40:08 -0300

    include and using fontAwesome icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MiCard());
@@ -10,91 +11,164 @@ class MiCard extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 46.0,
-              backgroundColor: Colors.red,
-              backgroundImage: AssetImage('images/profile.jpeg'),
-            ),
-            Text(
-              'Edilson',
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Pacifico',
-                letterSpacing: 2.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 46.0,
+                backgroundImage: AssetImage('images/profile.jpeg'),
               ),
-            ),
-            Text(
-              'Mobile && Web Developer',
-              style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'SourceSansPro',
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
+              Text(
+                'Edilson',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.0,
+                ),
               ),
-            ),
-            Text(
-              'Learning Dart Flutter',
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontFamily: 'Source Sans Pro',
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
+              Text(
+                'Mobile && Web Developer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Container(
-              color: Colors.black54,
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone_android,
-                    size: 20,
-                    color: Colors.black,
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Text(
-                    '+55 (85) 99754-5369',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+              Text(
+                'Learning Dart/Flutter',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Container(
-              color: Colors.black54,
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.whatsapp,
+                        size: 20,
+                        color: Colors.green,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        '+55 (85) 99754-5369',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Text(
-                    'edytavares66@gmail.com',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Source Sans Pro',
-                    ),
-                  )
-                ],
+                ),
               ),
-            )
-          ],
-        )),
+              Card(
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.envelope,
+                        size: 20,
+                        color: Colors.red,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'edytavares66@gmail.com',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.githubSquare,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Icon(
+                        FontAwesomeIcons.linkedin,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'edilson14',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 0),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.twitter,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        FontAwesomeIcons.instagram,
+                        color: Color.fromRGBO(255, 0, 0, 1),
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        '_edilson14',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Source Sans Pro',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
